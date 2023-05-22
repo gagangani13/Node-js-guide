8b8afc0e3bbccc4730e4ad42db49091ba7f7ae32
gagangani13 <gagangani17@gmail.com>
2023-05-22 15:34:41 +0530

    Writing and reading from a file

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
-const http=require('http');
-const server=http.createServer((request,response)=> {
-    console.log(request.url)
-    let msg=''
-    if (request.url==='/about'){
-        msg='About'
-    }else if(request.url==='/home'){
-        msg="Home"
-    }else if(request.url==='/node'){
-        msg='my Node Js project'
-    }else{
-        msg='website'
-    }
-    response.setHeader('Content-Type','text/html');
-    response.write('<html>');
-    response.write('<head><title>Backend</title></head>');
-    response.write(`<body><h1>Welcome to ${msg} </h1></body>`);
-    response.write('</html>');
-    response.end();
-    //you can't edit response after this it will give error
-})
-server.listen(3000)
+
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((request, response) => {
+  response.setHeader("Content-Type", "text/html");
+
+  if (request.url === "/") {
+    fs.readFile('message.txt',{encoding:'utf-8'},(err,data)=>{
+        if(err){
+            console.log(err);
+        }
+        response.write("<html>");
+        response.write("<head><title>Backend</title></head>");
+        response.write(`<body>`);
+        response.write("<h1>Welcome to my Node Js project </h1>");
+        response.write(`<h5>${data}</h5>`)
+        response.write(
+            "<form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>Send</button></form>"
+        );
+        response.write("/<body>");
+        response.write("</html>");
+        return response.end()
+    })
+  }
+  if (request.url === "/message" && request.method === "POST") {
+    const body = [];
+    request.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    request.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    response.statusCode = 302;
+    response.setHeader("Location", "/");
+    return response.end();
+  }
+  //you can't edit response after this it will give error
+});
+server.listen(3000);
