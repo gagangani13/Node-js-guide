e9c99309a43a79c7c9aa6d1fce2d494508493db5
gagangani13 <gagangani17@gmail.com>
2023-05-22 12:56:05 +0530

    Sending Response

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const http=require('http');
-function rqListener(request,response) {
-    console.log("GANI")//this gets called only when the browser loads
-}
-const server=http.createServer(rqListener)
+const server=http.createServer((request,response)=> {
+    console.log(request.url)
+    let msg=''
+    if (request.url==='/about'){
+        msg='About'
+    }else if(request.url==='/home'){
+        msg="Home"
+    }else if(request.url==='/node'){
+        msg='my Node Js project'
+    }else{
+        msg='website'
+    }
+    response.setHeader('Content-Type','text/html');
+    response.write('<html>');
+    response.write('<head><title>Backend</title></head>');
+    response.write(`<body><h1>Welcome to ${msg} </h1></body>`);
+    response.write('</html>');
+    response.end();
+    //you can't edit response after this it will give error
+})
 server.listen(3000)
