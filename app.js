3bf9524afcd91aac39c2f131cb8e3c7e3d626a83
gagangani13 <gagangani17@gmail.com>
2023-05-23 15:17:09 +0530

    Adding Route and Body parser

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-
-const express=require('express')
-const app=express()
-app.use((req,res,next)=>{  //Middleware
-    next()
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:false}))
+app.use("/addProduct", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='product'/><input type='text' name='description' /><button type='submit'>SEND</button></form>"
+  );
+});
+app.use('/product',(req, res, next)=>{
+    console.log(req.body)
+    res.redirect('/')
 })
-app.use((req,res,next)=>{
-    res.send('<h1>Welcome to Express</h1>')
-})
+app.use("/", (req, res, next) => {
+  res.send("<h1>Welcome to Express</h1>");
+});
 app.listen(3000);
